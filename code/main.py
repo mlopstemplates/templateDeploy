070f88279ce39bd77e60ceede2f2da12a61611f5
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-11 14:00:28 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -79,7 +79,8 @@ def main():
     tenant_id=azure_credentials.get("tenantId", "")
     service_principal_id=azure_credentials.get("clientId", "")
     service_principal_password=azure_credentials.get("clientSecret", "")
-    
+    print(service_principal_password)
+    print("here")
     command = ('az login --service-principal --username {APP_ID} --password {PASSWORD} --tenant {TENANT_ID}').format(
            APP_ID=service_principal_id, PASSWORD=service_principal_password, TENANT_ID=tenant_id)
     print(command)
