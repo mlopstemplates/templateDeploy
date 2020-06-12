93af99e60174936f6ff44c76d2c8a78d7f6f9c86
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-12 12:29:51 +0530

    Update test.py

--- code/test.py
+++ code/test.py
@@ -2,11 +2,11 @@ import os
 import sys
 import subprocess
  
-azure_credentials = { "clientId": "ab96606e-49a7-45d3-a575-5172e11fdb7f", "clientSecret": "^s:e6b4uCMXxN1|68t+i?[f](`E~8YeAP", "subscriptionId": "4847477c-3812-4667-ad10-174e0eab74d4", "tenantId": "2d1aba9c-5938-402b-90b9-72a284a4bced"}
+azure_credentials = { "clientId": "ab96606e-49a7-45d3-a575-5172e11fdb7f", "clientSecret": "^s:e6b4uCMXxN1|68t+i?[f](\\`E~8YeAP", "subscriptionId": "4847477c-3812-4667-ad10-174e0eab74d4", "tenantId": "2d1aba9c-5938-402b-90b9-72a284a4bced"}
  
 tenant_id=azure_credentials.get("tenantId", "")
 service_principal_id=azure_credentials.get("clientId", "")
-service_principal_password=azure_credentials.get("clientSecret", "").replace("`","\\`")
+service_principal_password=azure_credentials.get("clientSecret", "")
 command = ("az login --service-principal --username {APP_ID} --password \"{PASSWORD}\" --tenant {TENANT_ID}").format(
         APP_ID=service_principal_id, PASSWORD=service_principal_password, TENANT_ID=tenant_id)
  
