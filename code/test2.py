0811cd9d08682b37344d69558c8e2928c3c24d0a
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-12 16:10:40 +0530

    Update test2.py

--- code/test2.py
+++ code/test2.py
@@ -20,17 +20,17 @@ def main():
     temp = subprocess.Popen(['ls','-a'], stdout = subprocess.PIPE)
     print(temp)
     template_file_json = open(template_file_file_path, "w")
-    #json_object = json.load(template_file_json)
+    json_object = json.load(template_file_json)
     #json_object["parameters"]["subscriptionID"]["value"]=SubscriptionID
     #json_object["parameters"]["PatToken"]["value"]=PAT_TOKEN
     #json_object["parameters"]["GitRepo"]["value"]=Repo_NAME
     #json.dump(json_object, template_file_json) 
     #template_file_json.close() 
     
-    template_file_jsonR = open(template_file_file_path, "r")
-    json_objectR = json.load(template_file_jsonR)
-    template_file_jsonR.close() 
-    print(json_objectR)
+    #template_file_jsonR = open(template_file_file_path, "r")
+    #json_objectR = json.load(template_file_jsonR)
+    #template_file_jsonR.close() 
+    #print(json_objectR)
 
 if __name__ == "__main__":
     main()
