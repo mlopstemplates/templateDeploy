36e45bfe78bba86b0fce4542c9c27b7385b7034c
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-12 15:30:06 +0530

    Update test2.py

--- code/test2.py
+++ code/test2.py
@@ -8,14 +8,21 @@ from json import JSONDecodeError
 
 
 def main():
+    
     template_file = os.environ.get("INPUT_ARMTEMPLATE_FILE", default="deploya.json")
     template_params_file = os.environ.get("INPUT_ARMTEMPLATEPARAMS_FILE", default="deploy.parama.json")
     PAT_TOKEN="SAmpleToken"
     Repo_NAME="SampleRepo"
     SubscriptionID="SampleSubscription"
     template_file_file_path = os.path.join(".cloud", ".azure", template_params_file)
+    command='chmod+x '+template_file_file_path
+    try:
+       app_create = subprocess.check_output(command, shell=True)
+       print(app_create)
+    except Exception as ex:
+       print(ex)
     print(template_file_file_path)
-    os.chmod(template_file_file_path,stat.S_IRWXO)
+    #os.chmod(template_file_file_path,stat.S_IRWXO)
     template_file_json = open(template_file_file_path, "w")
     json_object = json.load(template_file_json)
     #json_object["parameters"]["subscriptionID"]["value"]=SubscriptionID
